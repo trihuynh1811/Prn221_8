c640f14e3fec93f132d070a66f592d14fa514497
triettruong14 <triettmce150047@fpt.edu.vn>
2024-02-23 22:45:31 +0700

    basic home page

---
+++ Prn221_8_HoaLan/Prn221_8_HoaLan/Pages/Home/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Prn221_8_HoaLan.Pages.Home
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
