e07448e6e38e70effed4dbf1dbb34c72afa290f0
trihuynh <77220677+trihuynh1811@users.noreply.github.com>
2024-03-03 13:53:42 +0700

    adjust ui in home page

---
+++ Prn221_8_HoaLan/Prn221_8_HoaLan/Pages/Products/ProductDisplay.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Prn221_8_HoaLan.Pages.Products
+{
+    public class ProductDisplayModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Prn221_8_HoaLan/Prn221_8_HoaLan/Pages/Products/ProductRegister.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Prn221_8_HoaLan.Pages.Products
+{
+    public class ProductRegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
