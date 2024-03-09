a5dc5c98175887a887146aa33d73d6e324c4156f
Ga Honghac <flamingotran1532002@gmail.com>
2024-03-09 10:34:12 +0700

    UPdate code manage product by staff

--- Prn221_8_HoaLan/Prn221_8_HoaLan/Pages/AuctionManagement/AuctionManagement.cshtml.cs
+++ Prn221_8_HoaLan/Prn221_8_HoaLan/Pages/AuctionManagement/AuctionManagement.cshtml.cs
@@ -1,52 +1,25 @@
 using DataAccessLayer.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository;
 
 namespace Prn221_8_HoaLan.Pages.AuctionManagement
 {
 
-  public class AuctionManagementModel : PageModel
-  {
-    [BindProperty]
-    public List<Product> Products { get; set; }
-
-    public IActionResult OnGet()
+    public class AuctionManagementModel : PageModel
     {
-      // Mock data
-      Products = new List<Product>{
-        new Product
-        {
-          ProductId = 1,
-                    Description = "Product 1 description",
-                    Image = "https://example.com/image1.jpg",
-                    Price = 10.0f,
-                    ProductName = "Product 1",
-                    Status = true,
-                    Quantity = 5
-        },
-          new Product
-          {
-            ProductId = 2,
-            Description = "Product 2 description",
-            Image = "https://example.com/image2.jpg",
-            Price = 20.0f,
-            ProductName = "Product 2",
-            Status = true,
-            Quantity = 10
-          },
-          new Product
-          {
-            ProductId = 3,
-            Description = "Product 3 description",
-            Image = "https://example.com/image3.jpg",
-            Price = 30.0f,
-            ProductName = "Product 3",
-            Status = true,
-            Quantity = 15
-          }
-      };
+        [BindProperty]
+        public List<Product> Products { get; set; }
 
-      return Page();
+        IProductRepository _iproduct;
+        public AuctionManagementModel(IProductRepository iProduct)
+        {
+            _iproduct = iProduct;
+        }
+        public IActionResult OnGet()
+        {
+            Products = _iproduct.GetAll();
+            return Page();
+        }
     }
-  }
 }
